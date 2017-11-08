e5cf907598577b69ffe15155c3b6c6efc69b281e
maruian@floridauniversitaria.es <maruian@floridauniversitaria.es>
2017-11-08 17:45:24 +0100

    Primer commit

---
+++ MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.a2dam.optionsmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case (R.id.ayuda):
+                Toast.makeText(getApplicationContext(),"Te ayudo",Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
